125090653f647bd37b5dfbad65b42efe0d019c4f
cjans121 <jpanthi@utexas.edu>
2025-05-14 15:42:37 -0600

    format to match plugin convention and add comments

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -1,10 +1,10 @@
 return {
-  {
-    "ellisonleao/gruvbox.nvim",
-    priority = 1000, -- Load before other plugins
-    config = function()
-      vim.o.background = "dark" -- or "light"
-      vim.cmd("colorscheme gruvbox")
-    end,
-  },
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000, -- Load before other plugins (for colorscheme)
+  config = function()
+    -- Set background and apply colorscheme after plugin is loaded
+    -- This ensures gruvbox is available in the runtimepath before calling colorscheme
+    vim.o.background = "dark" -- or "light"
+    vim.cmd("colorscheme gruvbox")
+  end,
 }
