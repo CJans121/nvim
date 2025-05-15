749a1044435dcd99bcf41008c87d4632acec5c0a
cjans121 <jpanthi@utexas.edu>
2025-05-15 15:47:50 -0600

    add treesitter setup

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,15 @@
 -- Provides advanced syntax highlighting, indentation, and code-aware features via nvim-treesitter (like incremental selection, folding, etc.)
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  build = ":TSUpdate"
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "lua", "vim", "c", "cpp", "python", "xml", "markdown", "bash"},
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+    })
+  end,
 }
