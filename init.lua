7960927681294e22d1c85724dc108d93ede5faeb
cjans121 <jpanthi@utexas.edu>
2025-05-15 16:08:58 -0600

    separate nvim config

--- init.lua
+++ init.lua
@@ -1,7 +1,2 @@
--- Redefine tab as two spaces
-vim.cmd("set expandtab")      -- Use spaces instead of tab characters
-vim.cmd("set tabstop=2")      -- Display existing tab characters as 2 spaces wide
-vim.cmd("set softtabstop=2")  -- Insert 2 spaces when Tab is pressed in insert mode
-vim.cmd("set shiftwidth=2")   -- Use 2 spaces for each step of auto-indent
-
+require("config.nvim")
 require("config.lazy")
